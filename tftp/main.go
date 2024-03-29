589e60fac7f7fd92e4a9d9f7cfe68023139ea8c8
wangbobo <wangbobo@intra.nsfocus.com>
2024-03-29 19:11:30 +0800

    refactor(tftp): 文件传输完成后释放文件句柄

---
+++ tftp/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/pin/tftp/v3"
+)
+
+func putFile(addr string, localPath string, remoteFilename string, mode string) error {
+	c, err := tftp.NewClient(addr)
+	if err != nil {
+		return fmt.Errorf("connecting %s: %v", addr, err)
+	}
+	file, err := os.Open(localPath)
+	if err != nil {
+		return fmt.Errorf("can't open %s: %v", localPath, err)
+	}
+	rf, err := c.Send(remoteFilename, mode)
+	if err != nil {
+		return fmt.Errorf("starting transfer: %v", err)
+	}
+	n, err := rf.ReadFrom(file)
+	if err != nil {
+		return fmt.Errorf("transferring %s: %v", remoteFilename, err)
+	}
+	fmt.Printf("%d bytes sent\n", n)
+	return nil
+}
+
+func getFile(addr string, localPath string, localFilename string, mode string) error {
+	c, err := tftp.NewClient(addr)
+	if err != nil {
+		return fmt.Errorf("connecting %s: %v", addr, err)
+	}
+	wt, err := c.Receive(localFilename, mode)
+	if err != nil {
+		return fmt.Errorf("requesting transfer: %v", err)
+	}
+	file, err := os.Create(localPath)
+	if err != nil {
+		return fmt.Errorf("creating %s: %v", localPath, err)
+	}
+	// Optionally obtain transfer size before actual data.
+	if n, ok := wt.(tftp.IncomingTransfer).Size(); ok {
+		fmt.Printf("Transfer size: %d\n", n)
+	}
+	n, err := wt.WriteTo(file)
+	if err != nil {
+		return fmt.Errorf("receiving %s: %v", localFilename, err)
+	}
+	fmt.Printf("%d bytes received.\n", n)
+	return nil
+}
+
+func main() {
+	addr := flag.String("a", "localhost:69", "Server address")
+	localPath := flag.String("l", "", "Local file path")
+	remoteFilename := flag.String("r", "", "Remote filename")
+	putOperation := flag.Bool("p", false, "Upload (aka PUT) transfer")
+	getOperation := flag.Bool("g", false, "Download (aka GET) transfer")
+	mode := flag.String("m", "octet", "Mode of transfer: 'octet' or 'netascii'")
+	flag.Parse()
+
+	if *localPath == "" {
+		fmt.Fprintf(os.Stderr, "Error: Local file path is missing.\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *remoteFilename == "" {
+		fmt.Fprintf(os.Stderr, "Error: Remote filename is missing.\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *mode != "netascii" && *mode != "octet" {
+		fmt.Fprintf(os.Stderr, "Invalid mode: %s\n\n", *mode)
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *getOperation && *putOperation {
+		fmt.Fprintf(os.Stderr, "Error: Upload and download at once is confusing. Choose one.\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *putOperation {
+		err := putFile(*addr, *localPath, *remoteFilename, *mode)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Uploading: %v\n", err)
+			os.Exit(3)
+		}
+	} else if *getOperation {
+		err := getFile(*addr, *localPath, *remoteFilename, *mode)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Downloading: %v\n", err)
+			os.Exit(2)
+		}
+	} else {
+		fmt.Fprintf(os.Stderr, "Error: Choose upload or download.\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+}
--- tftpd/main.go
+++ tftpd/main.go
@@ -1,52 +1,87 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/pin/tftp/v3"
 	"io"
 	"os"
 	"time"
+
+	"github.com/pin/tftp/v3"
 )
 
-// readHandler is called when client starts file download from server
+// Hander for read (aka GET) requests.
 func readHandler(filename string, rf io.ReaderFrom) error {
 	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+		fmt.Fprintf(os.Stderr, "opening %s: %v\n", filename, err)
 		return err
 	}
+	defer file.Close() // 在函数结束时关闭文件句柄
 	n, err := rf.ReadFrom(file)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+		fmt.Fprintf(os.Stderr, "reading %s: %v\n", filename, err)
 		return err
 	}
 	fmt.Printf("%d bytes sent\n", n)
 	return nil
 }
 
-// writeHandler is called when client starts file upload to server
+// Handler for write (aka PUT) requests.
 func writeHandler(filename string, wt io.WriterTo) error {
+	if _, err := os.Stat(filename); err == nil {
+		// 文件存在，尝试删除文件
+		err := os.Remove(filename)
+		if err != nil {
+			fmt.Println("无法删除文件:", err)
+			return err
+		}
+		fmt.Println("文件删除成功")
+	}
+
 	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0644)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+		fmt.Fprintf(os.Stderr, "creating %s: %v\n", filename, err)
 		return err
 	}
+	defer file.Close() // 在函数结束时关闭文件句柄
 	n, err := wt.WriteTo(file)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+		fmt.Fprintf(os.Stderr, "writing %s: %v\n", filename, err)
 		return err
 	}
 	fmt.Printf("%d bytes received\n", n)
 	return nil
 }
 
+// Hook for logging on every transfer completion or failure.
+type logHook struct{}
+
+func (h *logHook) OnSuccess(stats tftp.TransferStats) {
+	fmt.Printf("Transfer of %s to %s complete\n", stats.Filename, stats.RemoteAddr)
+}
+func (h *logHook) OnFailure(stats tftp.TransferStats, err error) {
+	fmt.Printf("Transfer of %s to %s failed: %v\n", stats.Filename, stats.RemoteAddr, err)
+}
+
 func main() {
-	// use nil in place of handler to disable read or write operations
+	port := flag.Int("p", 69, "Local port to listen")
+	flag.Parse()
+
+	// Start the server.
 	s := tftp.NewServer(readHandler, writeHandler)
-	s.SetTimeout(5 * time.Second) // optional
-	err := s.ListenAndServe(":69") // blocks until s.Shutdown() is called
-	if err != nil {
-		fmt.Fprintf(os.Stdout, "server: %v\n", err)
-		os.Exit(1)
-	}
+	s.SetHook(&logHook{})
+	go func() {
+		err := s.ListenAndServe(fmt.Sprintf(":%d", *port))
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "Can't start the server: %v\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// Do some other stuff.
+	time.Sleep(5000 * time.Minute)
+
+	// Eventually shutdown the server.
+	s.Shutdown()
 }
